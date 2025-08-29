a437578b55ba65da65aef8aee878e9c7321e4b49
Karmendra Deval <121801308+Karmendr@users.noreply.github.com>
2025-08-29 06:56:34 +0000

    28/08/2025

---
+++ Set_init.py
@@ -0,0 +1,16 @@
+# Set initialization 1
+
+# s = set({1,2,3,4,5})
+# print(s,end=" ")
+# print(type(s))
+
+# 1. -> Set is not contain index value.
+# 2. -> Set is con contain repetative value.
+
+# Set initialization 2
+# ceate empty set
+s = set()
+print(s)
+s.add(5)
+s.add(9)
+print(s)
