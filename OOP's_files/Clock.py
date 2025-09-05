9e2dd402d892bb268112f5fa3910cb0c1deea5b6
Karmendra Deval <121801308+Karmendr@users.noreply.github.com>
2025-09-05 09:07:12 +0000

    05/09/2025

--- OOP's_files/Clock.py
+++ OOP's_files/Clock.py
@@ -1,72 +1,69 @@
 import time
-<<<<<<< HEAD
-import os
-class Clock:
-  __h = 0
-  __m = 0
-  __s = 0
-  
-  # def __init__(self,h,m,s):
-  #   self.__h = h
-  #   self.__m = m
-  #   self.__s = s
-
-  def disp(self):
-    for i in range(0,23):
-      for j in range(0,59):
-        for k in range(0,59):
-          time.sleep(1)
-          os.system('cls')
-          return(f"{self.__h}:{self.__m}:{self.__s}")
-
-obj = Clock()
-print(obj.disp())
-=======
 
 
 class Clock:
 
-  def __init__(self, h, m, s):
-    self.__h = h
-    self.__m = m
-    self.__s = s
-    self.__day = 0
+  def __init__(self, h, m, s, am_pm):
+    if ((h >= 0 and h <= 12) and (m >= 0 and m <= 59)
+        and (s >= 0 and s <= 59)):
+      self.__h = h
+      self.__m = m
+      self.__s = s
+      self.__am_pm = am_pm
+      self.__am = "AM"
+      self.__pm = "PM"
+      self.__day = 0
+    else:
+      print("Please Enter Valid input")
 
   def disp(self):
     while (True):
-      for i in range(self.__h, 24):
+      for i in range(self.__h, 13):
         for j in range(self.__m, 60):
           for k in range(self.__s, 60):
-            # print(f"{i}:{j}:{k}","\n",end="")
-            if self.__day > 0:
-              print("%2dday:%2d:%2d:%2d" % (self.__day, i, j, k),
+            if self.__am_pm == self.__am:
+              print("%2dday :%2d:%2d:%2d %2s" %
+                    (self.__day, i, j, k, self.__am_pm),
                     "\r",
                     end="")
               time.sleep(1)
-              if (i == 23 and j == 59 and k == 59):
+              if (i == 12 and j == 59 and k == 59):
                 i = 0
                 j = 0
                 k = 0
                 self.__h = 0
                 self.__m = 0
                 self.__s = 0
-            else:
-              print("%2d:%2d:%2d" % (i, j, k), "\r", end="")
+                self.__am_pm = self.__pm
+                self.__day += 1
+            elif self.__am_pm == self.__pm:
+              print("%2dday %2d:%2d:%2d %2s" %
+                    (self.__day, i, j, k, self.__am_pm),
+                    "\r",
+                    end="")
               time.sleep(1)
-              if (i == 23 and j == 59 and k == 59):
+              if (i == 12 and j == 59 and k == 59):
                 i = 0
                 j = 0
                 k = 0
                 self.__h = 0
                 self.__m = 0
                 self.__s = 0
+                self.__am_pm = self.__am
+            else:
+              print("Please Enter Valid Input")
+              return
 
-      self.__day += 1
 
-
-h = int(input("Enter hour :- "))
-m = int(input("Enter minuts :- "))
-s = int(input("Enter seconds :- "))
-obj = Clock(h, m, s)
-obj.disp()
->>>>>>> 4e95930 (03/09/25)
+try:
+  h = int(input("Enter hour :- "))
+  m = int(input("Enter minuts :- "))
+  s = int(input("Enter seconds :- "))
+  try:
+    am_pm = input("AM or PM :- ")
+    obj = Clock(h, m, s, am_pm)
+    obj.disp()
+  except (ValueError):
+    print("Please Enter AM & PM in capital latter")
+except (ValueError):
+  print("Please Enter Valid input")
